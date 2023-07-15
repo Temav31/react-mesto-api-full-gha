e82b58fa8bbca9887445656b019a87954f426e90
Temav31 <temav331@mail.ru>
2023-07-15 07:44:01 +0300

    18

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,9 @@
-require('dotenv').config();
-const fs = require('fs');
-const https = require('https');
 const express = require('express');
+require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
-const helmet = require('helmet');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
-
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/work.tema.nomoredomains.work/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/work.tema.nomoredomains.work/fullchain.pem', 'utf8');
 const router = require('./routes/index');
 const errorHandler = require('./middlwares/error');
 const { requestLogger, errorLogger } = require('./middlwares/logger');
@@ -17,9 +11,8 @@ const corsError = require('./middlwares/cors');
 // константы
 const app = express();
 app.use('*', cors(corsError));
-// app.use(cors());
-app.use(helmet());
 app.use(express.json());
+// app.use(cors());
 const { PORT = 3000 } = process.env;
 // // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
@@ -29,7 +22,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.use(cookieParser());
 // логигер запросов
 app.use(requestLogger);
-// app.use(corsError);
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
@@ -41,9 +33,7 @@ app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 // порт
+app.listen(PORT);
 // app.listen(PORT, () => {
 //   console.log(`Порт: ${PORT}`);
 // });
-const httpsServer = https.createServer({ key: privateKey, cert: certificate }, app);
-
-httpsServer.listen(PORT);
