e07872627aab5996f325e3a96ada9b348d5598e4
Temav31 <temav331@mail.ru>
2023-07-15 10:30:12 +0300

    2

--- backend/app.js
+++ backend/app.js
@@ -11,19 +11,15 @@ const { requestLogger, errorLogger } = require('./middlwares/logger');
 // константы
 const app = express();
 app.use(express.json());
-// app.use(cors(corsError));
+// не получилось через файл передать свойства корсу(, только так
+// я не знаю почему так, может вы знаете?)
 app.use(cors({
   credentials: true,
   origin: [
-    // 'http://localhost:3001',
     'https://work.tema.nomoredomains.work',
     'http://work.tema.nomoredomains.work',
-    // 'https://api.work.tema.nomoredomains.work',
-    // 'http://api.work.tema.nomoredomains.work',
     'https://localhost:3001',
     'http://localhost:3001',
-    // 'https://localhost:3000',
-    // 'http://localhost:3000',
   ],
 }));
 const { PORT = 3000 } = process.env;
