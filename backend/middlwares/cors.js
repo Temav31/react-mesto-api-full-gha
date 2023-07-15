d0ed5a2db16ff455b1ae184f175de8efe85d247d
Temav31 <temav331@mail.ru>
2023-07-15 07:43:28 +0300

    18

--- backend/middlwares/cors.js
+++ backend/middlwares/cors.js
@@ -1,13 +1,13 @@
 module.exports.options = {
   origin: [ // Массив доменов, с которых разрешены кросс-доменные запросы.
-    // 'https://work.tema.nomoredomains.work/',
-    // 'http://work.tema.nomoredomains.work/',
-    'https://api.work.tema.nomoredomains.work/',
-    // 'http://api.work.tema.nomoredomains.work/',
-    // 'https://localhost:3001/',
-    // 'http://localhost:3001/',
-    // 'https://localhost:3000/',
-    // 'http://localhost:3000/',
+    'https://work.tema.nomoredomains.work',
+    'http://work.tema.nomoredomains.work',
+    'https://api.work.tema.nomoredomains.work',
+    'http://api.work.tema.nomoredomains.work',
+    'https://localhost:3001',
+    'http://localhost:3001',
+    'https://localhost:3000',
+    'http://localhost:3000',
   ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // настраивает заголовок CORS Access-Control-Allow-Methods.
   preflightContinue: false, // передать предварительный ответ CORS следующему обработчику.
